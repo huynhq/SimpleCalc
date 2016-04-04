2458862fc7844efc9a90c7dfa598ff04997cc866
Quynh Huynh <quhuynh@gmail.com>
2016-04-03 21:12:40 -0700

    Initial Commit

---
+++ SimpleCalc/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SimpleCalc
+//
+//  Created by Quynh Huynh on 4/3/16.
+//  Copyright © 2016 Quynh Huynh. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
